6a5a5626fc4cf17c75553179416836d33fbf241e
hiramfleitas <hiramfleitas@hotmail.com>
2018-12-01 01:29:22 -0500

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=sweetlou-Web-Deploy.pubxml -p:Password=y2LXwdwCgkfNGswoDyJWExhMzzvoGaGTL52KiDmfl024jv2k1Fggjd4PAtGo
+
